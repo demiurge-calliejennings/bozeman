1d36ccb5563122167583d3cd93e5298eaef11323
Will Jennings-Hess <will.jennings@demiurgestudios.com>
2015-04-18 20:08:02 -0400

    Testing express + React

--- index.js
+++ index.js
@@ -3,9 +3,7 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.get('/', function(request, response) {
-  response.send('Welcome to Bozeman!');
-});
+app.use(express.static('Client'));
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'));
