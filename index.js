5561c95051394f07e1c1be154a4498c0b9d2a456
Will Jennings-Hess <will.jennings@demiurgestudios.com>
2015-04-18 20:29:22 -0400

    Route from / to a particular React file

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static('Client'));
 
+app.get('/', function(request, response) {
+	response.sendFile(__dirname + '/Client/Hello-React.html');
+});
+
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'));
 });
