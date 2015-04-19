0a25c71cb4a4605885206747e77f5b1a0599d336
Will Jennings-Hess <will.jennings@demiurgestudios.com>
2015-04-18 23:54:17 -0400

    Use local copies of react libraries, instead of ones fetched from FB
    also update .gitignore & add react-dnd dependency.

--- index.js
+++ index.js
@@ -4,10 +4,13 @@ var app = express();
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static('Client'));
+app.use('/lib', express.static(__dirname + '/node_modules'));
 
 app.get('/', function(request, response) {
-	response.sendFile(__dirname + '/Client/Hello-React.html');
+	response.sendFile(__dirname + '/Client/Bozeman.html');
 });
+
+
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'));
